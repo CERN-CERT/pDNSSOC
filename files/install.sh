c46b49354b5c276f6b10d81d2e9a108fcb492a3d
romainw <github@wartel.net>
2022-12-08 08:24:53 -0700

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Cleaning the room."
+
 rm -f /etc/pdnssoc/pdnssoc.cron 2> /dev/null
 rm -f /etc/pdnssoc/pdnssoc.conf 2> /dev/null
 rm -f /usr/local/bin/pdnssoc.rb 2> /dev/null
@@ -7,17 +9,22 @@ rm -f /etc/pdnssoc/structure_html.txt 2> /dev/null
 rm -f /etc/cron.hourly/pdnssoc_misp 2> /dev/null
 rm -f /etc/td-agent/td-agent.conf 2> /dev/null
 
+echo "Installing system packages."
 # Packages installation
 yum -y install ruby git jq
 cd /tmp/
 git clone https://github.com/CERN-CERT/pDNSSOC/
 cd pDNSSOC/files
 
+echo "Installing Fluentd."
 curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent4.sh | sh
 td-agent-gem install fluent-plugin-filter-list
+
+echo "Installing Ruby dependencies."
 gem install parseconfig
 gem install misp
 
+echo "Moving pDNSSOC files around."
 # Fluentd 
 cp td-agent.conf /etc/td-agent/td-agent.conf
 
@@ -28,18 +35,27 @@ cp pdnssoc.rb /usr/local/bin/pdnssoc.rb
 cp pdnssoc.conf /etc/pdnssoc/pdnssoc.conf
 cp pdnssoc.cron /etc/pdnssoc/pdnssoc.cron
 
+echo "Installing pDNSSOC files."
+
 chmod +x /etc/pdnssoc/pdnssoc.cron
 chmod +x /usr/local/bin/pdnssoc.rb
 
+
 touch /etc/td-agent/misp_domains.txt
 ln -s /etc/pdnssoc/pdnssoc.cron /etc/cron.hourly/pdnssoc_misp
+
+echo  "*/1 * * * * root /usr/bin/ruby /usr/local/bin/pdnssoc.rb > /dev/null 2>&1" >> /etc/crontab
+
+echo "Running the first cron."
+
 /etc/pdnssoc/pdnssoc.cron
+
+echo "Starting Fluentd."
 
 # Start
 systemctl restart td-agent.service
 
-
-echo  "*/1 * * * * root /usr/bin/ruby /usr/local/bin/pdnssoc.rb > /dev/null 2>&1" >> /etc/crontab
+echo "Clearing install files."
 
 cd 
 rm -rf /tmp/pDNSSOC
