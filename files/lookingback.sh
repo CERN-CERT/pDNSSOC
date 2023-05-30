5ed41999bbc205ddf9ba911966ae79842bcc0c8d
romainw <github@wartel.net>
2023-05-30 11:04:01 +0200

    Update lookingback.sh

--- files/lookingback.sh
+++ files/lookingback.sh
@@ -6,5 +6,5 @@ alert_path=`grep alerts_path /etc/pdnssoc/pdnssoc.conf | awk -F "\"" '{print $4}
 
 zgrep  -F -f /etc/td-agent/misp_domains.txt /var/log/td-agent/queries.*.log* > ${temp_file}
 grep  -w -f /etc/td-agent/misp_domains.txt ${temp_file} >> ${alert_path}pdnssoc-pastlog.log
-cat ${temp_file}
+
 rm -f ${temp_file}
