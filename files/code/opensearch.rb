8424fbbe593c71d6e8b03d62fe836eb1ce3b19e6
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2023-07-21 19:19:26 +0200

    Add some code for opensearch

--- files/code/opensearch.rb
+++ files/code/opensearch.rb
@@ -7,13 +7,38 @@ class Opensearch
 
   def send_alert(results)
     begin
-    client = Opensearch::Client.new(
-      url: @@opensearch_config["server"],
-      username: @@opensearch_config["username"],
-      password: @@opensearch_config["password"],
-      log: true
-    )
+      client = Opensearch::Client.new(
+        url: @@opensearch_config["server"],
+        user: @@opensearch_config["username"],
+        password: @@opensearch_config["password"],
+        transport_options: { ssl: { verify: false } },
+        log: true
+      )
 
-    client.cluster.health
+      results.each do |ioc, data_ioc|
+        data_ioc["misp"].each_with_index do |misp_event, idx_event|
+          document = {
+            '@timestamp': Time.at(data_ioc["first_occurrence"]).strftime(TIME_FORMAT_YMD),
+            'source': 'pdnssoc',
+            'src_ip': data_ioc["client_ip"],
+            'ioc_detected': data_ioc["ioc_detected"],
+            'misp': {
+              'link': misp_event["misp_server"] + "/events/view/" + misp_event["misp_id"],
+              'misp_info': misp_event["misp_info"],
+              'num_iocs': misp_event["num_iocs"],
+              'publication': misp_event["publication"],
+              'organisation': misp_event["organisation"],
+              'comment': misp_event["comment"],
+              'tags': []
+            }
+          }
+
+          response = client.index(
+            index: @@opensearch_config["index"],
+            body: document,
+            refresh: true
+          )
+        end
+      end
     end
 end
